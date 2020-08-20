2ee28b422530c497bed0e4c86c75719828083edb
andres-boltdasarini <lau47@mail.ru>
2020-08-20 13:24:30 +0300

    toolbar

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
-
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
-
-const jsLoaders = () => {
-  const loaders = [
-    {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env']
-      }
-    }
-  ]
-
-  if (isDev) {
-    loaders.push('eslint-loader')
-  }
-
-  return loaders
-}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -86,7 +68,14 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: jsLoaders()
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
       }
     ]
   }

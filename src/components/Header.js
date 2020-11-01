1884d52ca88b549040ed0387e7d5c204b04f18f4
andres-boltdasarini <lau47@mail.ru>
2020-11-01 13:00:11 +0300

    start react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,63 @@
 import React from "react";
-import '../scss/dashboard.scss'
+import '../scss/header.scss'
+import '../scss/index.scss'
 
 const Header = () => {
-    return    <div>
-        <div className='db__header'>
-            <h1>My React App!</h1>
+    return (
+        <div className="excel">
+
+            <div className="excel__header">
+
+                <input type="text" className="input" value="Новая таблица"/>
+
+                <div>
+
+                    <div className="button">
+                        <i className="material-icons">delete</i>
+                    </div>
+
+                    <div className="button">
+                        <i className="material-icons">exit_to_app</i>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="excel__toolbar">
+
+                <div className="button">
+                    <i className="material-icons">format_align_left</i>
+                </div>
+
+                <div className="button">
+                    <i className="material-icons">format_align_center</i>
+                </div>
+
+                <div className="button">
+                    <i className="material-icons">format_align_right</i>
+                </div>
+
+                <div className="button">
+                    <i className="material-icons">format_bold</i>
+                </div>
+
+                <div className="button">
+                    <i className="material-icons">format_italic</i>
+                </div>
+
+                <div className="button">
+                    <i className="material-icons">format_underlined</i>
+                </div>
+
+            </div>
+
+            <div className="excel__formula">
+                <div className="info">fx</div>
+                <div className="input" contentEditable spellCheck="false"></div>
+            </div>
         </div>
-        <div className='db__new'>
-            <h1>My React App!</h1>
-        </div>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
-        <h1>My React App!</h1>
 
-    </div>
-
-
+    )
 }
 export default Header

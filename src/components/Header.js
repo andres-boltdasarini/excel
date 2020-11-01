ee2054988113af2e945ba670fd5391c2aa3dcbdd
andres-boltdasarini <lau47@mail.ru>
2020-11-01 12:32:10 +0300

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import '../scss/dashboard.scss'
+
+const Header = () => {
+    return    <div>
+        <div className='db__header'>
+            <h1>My React App!</h1>
+        </div>
+        <div className='db__new'>
+            <h1>My React App!</h1>
+        </div>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+        <h1>My React App!</h1>
+
+    </div>
+
+
+}
+export default Header

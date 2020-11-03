011e3ad64f513fbb8aad0c9acf286f41e56a36cc
andres-boltdasarini <lau47@mail.ru>
2020-11-03 20:25:25 +0300

    material-ui3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
-import '../scss/header.scss'
+
 import '../scss/index.scss'
-import Typography from '@material-ui/core/Typography';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-
+import Typography from '@material-ui/core/Typography';
 
 const Header = () => {
     return (
@@ -28,6 +27,161 @@ const Header = () => {
                 </div>
 
             </div>
+            <div className="excel__header__bar">
+                <div className='text'>
+                    <Typography>Galaxy S20</Typography>
+                </div>
+
+</div>
+            <div className="excel__header__bar2">
+                <div className='text2'>
+                    <Typography>Galaxy S20</Typography>
+                </div>
+            </div>
+
+            <div>
+                rwargfdgdgfd
+            </div>
+            <div>
+                rwar
+            </div>
+            <div>
+                rwar
+            </div>
+            <div>
+                rwar
+            </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>
+            v<div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>v
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div>v<div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div>
+            v<div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div><div>
+            rwar
+        </div>vv
+            <div>
+                rwar
+            </div><div>
+            rwar
+        </div>vvv<div>
+            rwar
+        </div><div>
+            rwar
+        </div>
+            v<div>
+            rwar
+        </div><div>
+            rwar
+        </div>vvv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         </div>

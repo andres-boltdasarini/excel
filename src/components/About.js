873a69944f977a9ad6798c59aebe163cc42691e3
andres-boltdasarini <lau47@mail.ru>
2020-11-08 13:03:57 +0300

    body1

---
+++ src/components/About.js
@@ -0,0 +1,168 @@
+import React from "react";
+import '../scss/about.scss'
+
+const About = () => {
+    return (
+        <div className='body'>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            v
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            v
+            <div>
+                test
+            </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div> <div>
+            test
+        </div>
+            <div>
+                test
+            </div>
+            <div>
+                test
+            </div> <div>
+            test
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
 import Header from "@/components/Header";
+import About from "@/components/About";
 
 
 class App extends Component {
     render() {
-        return <Header/>
+        return (
+            <div>
+                <Header/>
+                <About/>
+            </div>
+        )
     }
 }
 

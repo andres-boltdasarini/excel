630ed8828dcb821cee9d41521932d0995155a7f3
andres-boltdasarini <lau47@mail.ru>
2020-11-09 21:07:53 +0300

    body2

---
+++ src/components/Support.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../scss/about.scss'
+
+const Support = () => {
+    return (
+        <div className='body'>
+            <h1>Поддержка</h1>
+            <div>
+                Вых с помощью цмартбильного телефона.
+                <br/>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+
+            <br/>
+            <br/>
+            <div>
+                *Функция 8K Video Snap позволяет превращать кадры 8K видео в великолепные фотоснимки с разрешением 33 МП.
+            </div>
+            <br/>
+            <br/>
+        </div>
+    )
+}
+
+
+export default Support

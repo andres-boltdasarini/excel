193fc512ed059ef11db52159f14f65ed785cdbf0
andres-boltdasarini <lau47@mail.ru>
2020-11-21 21:17:07 +0300

    add experimental syntax support

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,8 @@ module.exports = {
             loader: 'babel-loader',
             options: {
               presets: [ "@babel/preset-env",
-                "@babel/preset-react"]
+                "@babel/preset-react",{
+                  'plugins': ['@babel/plugin-proposal-class-properties']}]
             }
           }
         ]
